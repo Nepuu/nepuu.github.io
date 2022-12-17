38d88c632187926bf761aa2cdddc0bc4cbf9a916
Nepuu <118331326+Nepuu@users.noreply.github.com>
2022-12-17 14:03:33 +0700

    Add files via upload

--- asset/function.js
+++ asset/function.js
@@ -1,12 +1,33 @@
+alert ("Jangan di-refresh, nanti disuruh Login lagi.");
+
 function UserLogIn () {
     var Username = document.getElementById ("Username").value;
+    document.getElementById ("Pw").type = "password";
+
+    function MainPageLoad () {
+        document.getElementById ("Main-Content-Login").style.display = "none";
+        document.getElementById ("Main-Content-Loading").style.display = "none";
+        document.getElementById ("Main-Content-SourceCode").style.display = "block";
+    }
 
     if (Username == "Vohi5ma") {
-        alert ("Log-In Berhasil, Selamat datang !");
         document.getElementById ("Main-Content-Login").style.display = "none";
-        document.getElementById ("Main-Content-SourceCode").style.display = "block";
+        document.getElementById ("Main-Content-Loading").style.display = "block";
+        setInterval (MainPageLoad, 1000);
     } else {
         alert ('Username ' + '"' + Username + '" tidak valid.');
         document.getElementById ("Username").value = "";
     }
+}
+
+function ShowPassword () {
+    document.getElementById ("Pw").type = "text";
+    document.getElementById ("Pw-Show").style.display = "none";
+    document.getElementById ("Pw-Hide").style.display = "block";
+}
+
+function HidePassword () {
+    document.getElementById ("Pw").type = "password";
+    document.getElementById ("Pw-Hide").style.display = "none";
+    document.getElementById ("Pw-Show").style.display = "block";
 }
